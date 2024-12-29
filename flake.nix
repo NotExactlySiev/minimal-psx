f687681f362c95e2a049a2ae4993f5ff3311f36b
NotExactlySiev <sadrabousi@gmail.com>
2024-12-29 18:14:03 +0330

    nix flake updated to work for all systems

--- flake.nix
+++ flake.nix
@@ -7,22 +7,29 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, pcsx-redux }:
-    let
-      pkgs = import nixpkgs {
-        localSystem = { system = builtins.currentSystem; };
-      };
-      crossPkgs = import nixpkgs {
-        localSystem = { system = builtins.currentSystem; };
-        crossSystem = { system = "mipsel-none-elf"; };
-      };
-    in
+  outputs = { self, nixpkgs }:
+  let
+    lib = nixpkgs.lib;
+    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+  in
+    {
+      devShells = forAllSystems (system:
+      let
+        pkgs = import nixpkgs {
+          localSystem = { system = system; };
+        };
+        crossPkgs = import nixpkgs {
+          localSystem = { system = system; };
+          crossSystem = { system = "mipsel-none-elf"; };
+        };
+      in
       {
-        devShells.x86_64-linux.default = pkgs.mkShell {
+        default = pkgs.mkShell {
           packages = [
             crossPkgs.buildPackages.gcc-unwrapped
             crossPkgs.buildPackages.binutils-unwrapped
           ];
         };
-      };
+      });
+    };
 }
